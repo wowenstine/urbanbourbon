638e7708b1336bbe7964b2dc007d2e3d9fadebfc
“Rafael <rafael.freaner@gmail.com>
2014-08-11 14:27:42 -0400

    Implemented Monban and Monban Generators, Visitor can now sign up and sign in as a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
+require "monban/constraints/signed_in"
+require "monban/constraints/signed_out"
+
 Rails.application.routes.draw do
+  constraints Monban::Constraints::SignedIn.new do
+    root "dashboards#show", as: :dashboard 
+  end
+
+  constraints Monban::Constraints::SignedOut.new do
+    root "homes#show"
+  end
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  resource :dashboard, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
