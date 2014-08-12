373e4c2ab29d584997f3bcc2415fefef7732251b
“Rafael <rafael.freaner@gmail.com>
2014-08-12 11:19:09 -0400

    Admin can now create bourbonpages, visitors can view featured bourbons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :bourbons
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :bourbons, only: [:new, :create, :show, :index]
   resource :dashboard, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
