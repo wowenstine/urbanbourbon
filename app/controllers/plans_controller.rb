ed92dae8d7cf089ed72daa0b19dbc73014827f03
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-14 16:24:52 -0400

    Fix white space

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,4 @@
 class PlansController < ApplicationController
-
   def show
     @plan = Plan.new
   end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,4 @@
 class Plan < ActiveRecord::Base
-  has_many  :subscriptions
+  has_many :subscriptions
   validates :amount, presence: true
 end
